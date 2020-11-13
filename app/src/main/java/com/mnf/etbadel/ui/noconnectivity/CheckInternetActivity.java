d4f2000d899c1bc4d9f4e97e449a1c0669497eda
Nishikant Shinde <nishiknatshinde007@gmail.com>
2020-11-13 05:40:53 +0530

    No internet connection activity design.

---
+++ app/src/main/java/com/mnf/etbadel/ui/noconnectivity/CheckInternetActivity.java
@@ -0,0 +1,15 @@
+package com.mnf.etbadel.ui.noconnectivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.mnf.etbadel.R;
+
+import android.os.Bundle;
+
+public class CheckInternetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_internet);
+    }
+}
